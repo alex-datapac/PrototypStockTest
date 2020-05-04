b1a24a9af3a61777f33a5eb3de7fc9aaeec9f526
Alexander Szalonnás <alexander.szalonnas@datapac.sk>
2020-05-04 15:22:15 +0200

    test

---
+++ webapp/controller/DetailPage1_1.controller.js
@@ -0,0 +1,332 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./Dialog3", "./Dialog5",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Dialog3, Dialog5, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.purchaseOrders.controller.DetailPage1_1", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5e78df38425c300121d92acd";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			if (!this.sContext) {
+				this.sContext = "PurchaseOrderSet('1111122222')";
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onPageNavButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("MasterPage1", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var sDialogName = "Dialog5";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog5(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onRowPress: function(oEvent) {
+
+			var sDialogName = "Dialog3";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog3(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onUploadCollectionUploadComplete: function(oEvent) {
+
+			var oFile = oEvent.getParameter("files")[0];
+			var iStatus = oFile ? oFile.status : 500;
+			var sResponseRaw = oFile ? oFile.responseRaw : "";
+			var oSourceBindingContext = oEvent.getSource().getBindingContext();
+			var sSourceEntityId = oSourceBindingContext ? oSourceBindingContext.getProperty("ID") : null;
+			var oModel = this.getView().getModel();
+
+			return new Promise(function(fnResolve, fnReject) {
+				var fnSync = function() {
+					oModel.detachRequestCompleted(fnRefreshCompleted);
+					oModel.detachRequestFailed(fnRefreshFailed);
+				};
+				var fnRefreshCompleted = function() {
+					fnSync();
+					fnResolve();
+				};
+				var fnRefreshFailed = function() {
+					fnSync();
+					fnReject(new Error("refresh failed"));
+				};
+				var fnResolveAfterRefresh = function() {
+					oModel.attachRequestCompleted(fnRefreshCompleted);
+					oModel.attachRequestFailed(fnRefreshFailed);
+					oModel.refresh(true, true);
+				};
+
+				if (iStatus !== 200) {
+					fnReject(new Error("Upload failed"));
+				} else if (oModel.hasPendingChanges()) {
+					fnReject(new Error("Please save your changes, first"));
+				} else if (!sSourceEntityId) {
+					fnReject(new Error("No source entity key"));
+				} else {
+					try {
+						var oResponse = JSON.parse(sResponseRaw);
+						var oNewEntityInstance = {};
+
+						oNewEntityInstance[""] = oResponse["ID"];
+						oNewEntityInstance[""] = sSourceEntityId;
+						oModel.createEntry("", {
+							properties: oNewEntityInstance
+						});
+						oModel.submitChanges({
+							success: function(oResponse) {
+								var oChangeResponse = oResponse.__batchResponses[0].__changeResponses[0];
+								if (oChangeResponse && oChangeResponse.response) {
+									oModel.resetChanges();
+									fnReject(new Error(oChangeResponse.message));
+								} else {
+									fnResolveAfterRefresh();
+								}
+							},
+							error: function(oError) {
+								fnReject(new Error(oError.message));
+							}
+						});
+					} catch (err) {
+						var message = typeof err === "string" ? err : err.message;
+						fnReject(new Error("Error: " + message));
+					}
+				}
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onUploadCollectionChange: function(oEvent) {
+
+			var oUploadCollection = oEvent.getSource();
+			var aFiles = oEvent.getParameter('files');
+
+			if (aFiles && aFiles.length) {
+				var oFile = aFiles[0];
+				var sFileName = oFile.name;
+
+				var oDataModel = this.getView().getModel();
+				if (oUploadCollection && sFileName && oDataModel) {
+					var sXsrfToken = oDataModel.getSecurityToken();
+					var oCsrfParameter = new sap.m.UploadCollectionParameter({
+						name: "x-csrf-token",
+						value: sXsrfToken
+					});
+					oUploadCollection.addHeaderParameter(oCsrfParameter);
+					var oContentDispositionParameter = new sap.m.UploadCollectionParameter({
+						name: "content-disposition",
+						value: "inline; filename=\"" + encodeURIComponent(sFileName) + "\""
+					});
+					oUploadCollection.addHeaderParameter(oContentDispositionParameter);
+				} else {
+					throw new Error("Not enough information available");
+				}
+			}
+
+		},
+		_onUploadCollectionTypeMissmatch: function() {
+			return new Promise(function(fnResolve) {
+				sap.m.MessageBox.warning("The file you are trying to upload does not have an authorized file type (JPEG, JPG, GIF, PNG, TXT, PDF, XLSX, DOCX, PPTX).", {
+					title: "Invalid File Type",
+					onClose: function() {
+						fnResolve();
+					}
+				});
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err);
+				}
+			});
+
+		},
+		_onUploadCollectionFileSizeExceed: function() {
+			return new Promise(function(fnResolve) {
+				sap.m.MessageBox.warning("The file you are trying to upload is too large (10MB max).", {
+					title: "File Too Large",
+					onClose: function() {
+						fnResolve();
+					}
+				});
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err);
+				}
+			});
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailPage1_1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("MasterPage1", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		},
+		onExit: function() {
+
+			// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
+			var aControls = [{
+				"controlId": "sap_IconTabBar_Page_0-content-sap_m_IconTabBar-2-items-sap_m_IconTabFilter-1-content-build_simple_Table-1585152210804",
+				"groups": ["items"]
+			}, {
+				"controlId": "sap_IconTabBar_Page_0-content-sap_m_IconTabBar-2-items-sap_m_IconTabFilter-5-content-sap_m_Panel-1585234994062-content-build_simple_Table-1",
+				"groups": ["items"]
+			}, {
+				"controlId": "sap_IconTabBar_Page_0-content-sap_m_IconTabBar-2-items-sap_m_IconTabFilter-3-content-sap_m_Panel-1585068693929-content-build_simple_Table-1585070493331",
+				"groups": ["items"]
+			}];
+			for (var i = 0; i < aControls.length; i++) {
+				var oControl = this.getView().byId(aControls[i].controlId);
+				if (oControl) {
+					for (var j = 0; j < aControls[i].groups.length; j++) {
+						var sAggregationName = aControls[i].groups[j];
+						var oBindingInfo = oControl.getBindingInfo(sAggregationName);
+						if (oBindingInfo) {
+							var oTemplate = oBindingInfo.template;
+							oTemplate.destroy();
+						}
+					}
+				}
+			}
+
+		}
+	});
+}, /* bExport= */ true);
